2f5f36897d9d3078c7d631bad9961ef8b1f796fd
jacky09299 <66111556+jacky09299@users.noreply.github.com>
2024-03-22 17:51:11 +0800

    Create compare_similarity.py

---
+++ Yolo/compare_similarity.py
@@ -0,0 +1,22 @@
+import torch
+import open_clip
+import cv2
+from sentence_transformers import util
+from PIL import Image
+# image processing model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-16-plus-240', pretrained="laion400m_e32")
+model.to(device)
+def imageEncoder(img):
+    img1 = Image.fromarray(img).convert('RGB')
+    img1 = preprocess(img1).unsqueeze(0).to(device)
+    img1 = model.encode_image(img1)
+    return img1
+def generateScore(image1, image2):
+    test_img = cv2.imread(image1, cv2.IMREAD_UNCHANGED)
+    data_img = cv2.imread(image2, cv2.IMREAD_UNCHANGED)
+    img1 = imageEncoder(test_img)
+    img2 = imageEncoder(data_img)
+    cos_scores = util.pytorch_cos_sim(img1, img2)
+    score = round(float(cos_scores[0][0])*100, 2)
+    return score
