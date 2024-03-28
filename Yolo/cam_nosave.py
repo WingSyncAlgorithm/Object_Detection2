9c00e6125a869bed6de252315fdc22fe4c18cb7a
jacky09299 <66111556+jacky09299@users.noreply.github.com>
2024-03-28 12:50:55 +0800

    Update cam_nosave.py

--- Yolo/cam_nosave.py
+++ Yolo/cam_nosave.py
@@ -3,14 +3,7 @@ import os
 
 def record_video():
     # 設定影像儲存路徑
-    """
-    save_path = r'C:\Users\jacky\Videos\cam\frames2'
-    video_save_path = r'C:\Users\jacky\Videos\cam\output2.avi'
-
-    # 創建儲存影像的資料夾
-    if not os.path.exists(save_path):
-        os.makedirs(save_path)
-    """
+    
 
     # 打開相機
     cap = cv2.VideoCapture(0)
@@ -24,12 +17,6 @@ def record_video():
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    """
-    # 設定視訊寫入器
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter(video_save_path, fourcc, 20.0, (width, height))
-    """
-
     frame_count = 0
 
     while cap.isOpened():
@@ -39,14 +26,6 @@ def record_video():
         if ret:
             # 顯示影像
             cv2.imshow('frame', frame)
-
-            """
-            # 寫入影像到 VideoWriter 對象
-            out.write(frame)
-
-            # 將每一幀影像保存到資料夾中
-            cv2.imwrite(os.path.join(save_path, f'frame_{frame_count}.jpg'), frame)
-            """
 
             frame_count += 1
 
